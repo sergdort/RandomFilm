0849d7a9ab3dc7c22185850a219320a8cd9cc827
sergdort <sergdort@gmail.com>
2017-07-25 22:27:26 +0100

    create proper package

--- app/src/main/java/com/randomfilm/sergdort/scenes/randomfilms/RandomFilmActivity.kt
+++ app/src/main/java/com/randomfilm/sergdort/scenes/randomfilms/RandomFilmActivity.kt
@@ -1,10 +1,10 @@
-package com.randofilm.sergdort.randomfilm
+package com.randomfilm.sergdort.scenes.randomfilms
 
 import android.os.Bundle
 import com.jakewharton.rxbinding2.widget.textChangeEvents
+import com.randofilm.sergdort.randomfilm.R
 import com.randomfilm.sergdort.domain.Networking.APIProvider
 import com.randomfilm.sergdort.extensions.takeUntilDestroyOf
-import com.trello.rxlifecycle2.android.ActivityEvent
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
